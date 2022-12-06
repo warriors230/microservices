a7ceea7bda5c182a630c933c6687b86869f8a18f
warriors230 <baironduvan1998@hotmail.com>
2022-12-06 16:31:09 -0500

    I18NV2
    Segunda integracion

---
+++ i18N/src/main/java/com/warriors/i18N/I18NApplication.java
@@ -0,0 +1,13 @@
+package com.warriors.i18N;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class I18NApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(I18NApplication.class, args);
+	}
+
+}
---
+++ i18N/src/test/java/com/warriors/i18N/I18NApplicationTests.java
@@ -0,0 +1,13 @@
+package com.warriors.i18N;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class I18NApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
