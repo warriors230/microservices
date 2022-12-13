42fff454dcdd28ab272fcd2df8159e99c993698e
warriors230 <baironduvan1998@hotmail.com>
2022-12-13 10:27:13 -0500

    Main
    config service

---
+++ config-service/src/test/java/com/warriors/configservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.warriors.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
